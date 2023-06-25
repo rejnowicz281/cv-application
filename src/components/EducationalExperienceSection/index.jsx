f61b97b4b4d594758c2513f25a55f15db54c82c9
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-06-25 16:36:51 +0200

    Create a new FormSection component and make educationalExperience inherit from it

--- src/components/EducationalExperienceSection/index.jsx
+++ src/components/EducationalExperienceSection/index.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
-import { Component } from "react";
+import FormSection from "../FormSection";
+
 {
     /* School name, title of study, date of study */
 }
-export default class EducationalExperienceSection extends Component {
+export default class EducationalExperienceSection extends FormSection {
     constructor(props) {
         super(props);
 
@@ -40,8 +41,7 @@ export default class EducationalExperienceSection extends Component {
         });
     }
 
-    onSubmit(e) {
-        e.preventDefault();
+    onSubmit() {
         this.props.onSubmit(this.state.newEntry);
         this.setState({ adding: false, newEntry: { schoolName: "", titleOfStudy: "", dateOfStudy: "" } });
     }
@@ -49,7 +49,7 @@ export default class EducationalExperienceSection extends Component {
     render() {
         return (
             <section className="EducationalExperienceSection">
-                <h1>Educational experience</h1>
+                {this.header("Educational Experience")}
                 <ul>
                     {this.props.educationalExperience.map((exp, idx) => {
                         return (
@@ -59,45 +59,26 @@ export default class EducationalExperienceSection extends Component {
                         );
                     })}
                 </ul>
-                {this.state.adding ? (
-                    <form onSubmit={(e) => this.onSubmit(e)} className="d-flex gap-1">
-                        <input
-                            onChange={(e) => this.setSchoolName(e.target.value)}
-                            type="text"
-                            value={this.state.newEntry.schoolName}
-                            placeholder="School name"
-                            required
-                        />
-                        <input
-                            onChange={(e) => this.setTitleOfStudy(e.target.value)}
-                            type="text"
-                            value={this.state.newEntry.titleOfStudy}
-                            placeholder="Title of study"
-                            required
-                        />
-                        <input
-                            onChange={(e) => this.setDateOfStudy(e.target.value)}
-                            type="date"
-                            value={this.state.newEntry.dateOfStudy}
-                            placeholder="Date of study"
-                            required
-                        />
-                        <button type="submit" className="btn btn-primary">
-                            Add
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => this.setState({ adding: false })}
-                            className="btn btn-danger"
-                        >
-                            Cancel
-                        </button>
-                    </form>
-                ) : (
-                    <button onClick={() => this.setState({ adding: true })} className="btn btn-primary mt-3">
-                        Add
-                    </button>
-                )}
+                {this.form([
+                    {
+                        type: "text",
+                        placeholder: "School name",
+                        value: this.state.newEntry.schoolName,
+                        onChange: (value) => this.setSchoolName(value),
+                    },
+                    {
+                        type: "text",
+                        placeholder: "Title of study",
+                        value: this.state.newEntry.titleOfStudy,
+                        onChange: (value) => this.setTitleOfStudy(value),
+                    },
+                    {
+                        type: "date",
+                        placeholder: "Date of study",
+                        value: this.state.newEntry.dateOfStudy,
+                        onChange: (value) => this.setDateOfStudy(value),
+                    },
+                ])}
             </section>
         );
     }
